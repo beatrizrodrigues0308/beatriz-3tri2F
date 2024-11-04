23d11f1689cc3b9fe148262c2630525eba874bb9
beatrizrodrigues0308 <160124987+beatrizrodrigues0308@users.noreply.github.com>
2024-11-04 11:41:09 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,19 @@
-function name(tema,pergunta,resposta) {
+function criaCartao(tema,pergunta,resposta) {
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className =  'cartao';
+    cartao.innerHTML = `
+    <div class="cartao-conteudo">
+        <h3>${tema}</h3>
+        <div class="cartao-conteudo-pergunta">
+            ${pergunta}
+        </div>
+        <div class="cartao-conteudo-resposta">
+            ${resposta}
+        </div>
+    </div> `
+    container.appendChild(cartao)
     
 }
+criaCartao('História','Em que ano iniciou-se a guerra do Vietnã?','Em 1955')
+criaCartao('alfabeto','letra depois do A?','B')
